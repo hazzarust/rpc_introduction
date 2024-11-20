827bdfd1faf55cc45c25e0acc33578ab4cf2ad7c
hazzarust <itshazzag@gmail.com>
2024-11-20 01:02:54 +0000

    made project file

---
+++ rpc/target/debug/deps/rpc-4b0fac35d80d596c.d
@@ -0,0 +1,5 @@
+/Users/hazza/projects/rpc_intro/rpc/target/debug/deps/rpc-4b0fac35d80d596c: src/main.rs
+
+/Users/hazza/projects/rpc_intro/rpc/target/debug/deps/rpc-4b0fac35d80d596c.d: src/main.rs
+
+src/main.rs:
---
+++ rpc/target/debug/rpc.d
@@ -0,0 +1,1 @@
+/Users/hazza/projects/rpc_intro/rpc/target/debug/rpc: /Users/hazza/projects/rpc_intro/rpc/src/main.rs
